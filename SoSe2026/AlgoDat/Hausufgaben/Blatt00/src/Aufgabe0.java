38f46bee601595def4ccef9c70cb0b241c8acba3
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2026-04-17 12:27:23 +0200

    Füge neue Kursmaterialien und Arbeitsblätter für SoSe2026 hinzu, aktualisiere README-Dateien und Workflow für AlgoDat

---
+++ SoSe2026/AlgoDat/Hausufgaben/Blatt00/src/Aufgabe0.java
@@ -0,0 +1,25 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Aufgabe0 {
+
+	public void print() throws FileNotFoundException {
+
+		Scanner in = new Scanner(new FileInputStream("Abgaben.txt"));
+
+		while (in.hasNext()) {
+			System.out.println(in.nextLine());
+		}
+		in.close();
+	}
+
+  public static void main(String[] args) {
+    try {
+      new Aufgabe0().print();
+    } catch (FileNotFoundException e) {
+      System.err.printf("Not Printeble! %s\n", e.toString());
+    }
+  }
+}
+
