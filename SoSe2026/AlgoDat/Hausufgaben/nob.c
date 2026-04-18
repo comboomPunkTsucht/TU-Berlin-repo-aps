fe2afdc12a3d9a2bf6497abfd57fd630abe5aa65
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2026-04-18 20:12:17 +0200

    .
    Signed-off-by: mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>

--- SoSe2026/AlgoDat/Hausufgaben/nob.c
+++ SoSe2026/AlgoDat/Hausufgaben/nob.c
@@ -9,10 +9,11 @@
 Nob_Cmd cmd = { 0 };
 
 int     blatt00( ) {
-  nob_log(NOB_INFO, "gehe zur Aufgabn von Blatt00");
+  nob_log( NOB_INFO, "Blatt00:" );
+  nob_log( NOB_INFO, "gehe zu den Aufgabn von Blatt00" );
   set_current_dir( "Blatt00/src" );
 
-  nob_log(NOB_INFO, "kompiliere die Aufgabe0.java");
+  nob_log( NOB_INFO, "kompiliere die Aufgabe0.java" );
 
   cmd_append( &cmd, "javac" );
   cmd_append( &cmd, "Aufgabe0.java" );
@@ -20,7 +21,7 @@ int     blatt00( ) {
     return 1;
   }
 
-  nob_log(NOB_INFO, "führe die Aufgabe0.java aus");
+  nob_log( NOB_INFO, "führe die Aufgabe0.java aus" );
 
   cmd_append( &cmd, "java" );
   cmd_append( &cmd, "Aufgabe0.java" );
@@ -28,36 +29,33 @@ int     blatt00( ) {
     return 1;
   }
 
-  nob_log(NOB_INFO, "Aufgabe0.java erfolgreich ausgeführt");
+  nob_log( NOB_INFO, "Aufgabe0.java erfolgreich ausgeführt" );
 
   return 0;
 }
 
-int main(int argc, char **argv)
-{
+int main( int argc, char **argv ) {
   NOB_GO_REBUILD_URSELF( argc, argv );
 
   nob_set_log_handler( nob_cancer_log_handler );
 
-    char* blatt;
+  if ( argc < 2 ) {  // <-- Geändert von > auf <
+    nob_log( NOB_ERROR, "Usage: %s [Blatt]", argv[ 0 ] );
+    return 1;
+  }
 
-    if ( argc < 2 ) {  // <-- Geändert von > auf <
-      fprintf( stderr, "Usage: %s [Aufgabe]\n", argv[ 0 ] );
+  String_View blatt = sv_from_cstr( argv[ 1 ] );
+
+  if ( sv_eq( blatt, sv_from_cstr( "alle" ) ) ) {
+    if ( blatt00( ) ) {
       return 1;
     }
 
-      blatt = strdup( argv[ 1 ] );
+  } else if ( sv_eq( blatt, sv_from_cstr( "Blatt00" ) ) ) {
+    return blatt00( );
+  } else {
+    UNREACHABLE( "keine gültige Aufgabe angegeben" );
+  }
 
-      if ( sv_eq( sv_from_cstr( blatt ), sv_from_cstr( "all" ) ) ) {
-        if ( blatt00( ) ) {
-          return 1;
-        }
-
-      } else if ( sv_eq( sv_from_cstr( blatt ), sv_from_cstr( "Blatt00" ) ) ) {
-          return blatt00( );
-        } else {
-          UNREACHABLE( "keine gültige Aufgabe angegeben" );
-        }
-
-    return 0;
+  return 0;
 }
